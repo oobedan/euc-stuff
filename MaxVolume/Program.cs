259d8a9e31cadb74e5671ce04a862a82320592b1
Dan <64520986+oobedan@users.noreply.github.com>
2026-05-18 22:11:58 +1000

    MaxVolume
    C# Console App to force the volume of default mic to 100%

---
+++ MaxVolume/Program.cs
@@ -0,0 +1,31 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Data;
+
+namespace MicVolumeSetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Get default recording device (microphone)
+                MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+                MMDevice mic = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Multimedia);
+
+                // Set microphone volume to 100%
+                mic.AudioEndpointVolume.MasterVolumeLevelScalar = 1.0f;
+
+                // Console.WriteLine($"Microphone volume set to 100%");
+            }
+            catch (Exception ex)
+            {
+                // Console.WriteLine("Error: " + ex.Message);
+            }
+
+            //Console.WriteLine("Press any key to exit...");
+            //Console.ReadKey();
+        }
+    }
+}
